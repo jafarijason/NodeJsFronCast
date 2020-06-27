6bc896675c14313371799cdea6be3fac9cab6a5b
thisisjasonjafari <jason.jafari.m@gmail.com>
2020-06-27 17:22:55 -0400

    make add-product views

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,12 @@
+const path = require('path')
+
 const express = require('express')
 
 const router = express.Router()
 
 router.get("/add-product", (req, res) => {
-    res.send("add-product");
+    // res.send("add-product");
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
 });
 
 module.exports = router
