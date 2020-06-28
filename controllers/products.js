dfa041744d298847b7a91a3c84ff1f8f27b991b1
thisisjasonjafari <jason.jafari.m@gmail.com>
2020-06-28 01:42:56 -0400

    make class on products

--- controllers/products.js
+++ controllers/products.js
@@ -1,21 +1,22 @@
-const products = [];
+const Product = require("../models/single-product");
 
 module.exports.addProductPage = (req, res) => {
     res.render("add-product", {
-        pageTitle: 'مدیریت',
+        pageTitle: "مدیریت",
     });
-}
-
+};
 
 module.exports.sendAllProducts = (req, res) => {
-    products.push({ title: req.body.title });
+    // products.push({ title: req.body.title });
+    const products = new Product(req.body.title);
+    products.saveProductData()
     res.redirect("/");
-}
-
+};
 
 module.exports.getAllProducts = (req, res) => {
-    res.render('shop', {
-        pageTitle: 'فروشگاه',
-        productsArray: products
-    })
-}
+    const products = Product.fetchAllProducts()
+    res.render("shop", {
+        pageTitle: "فروشگاه",
+        productsArray: products,
+    });
+};
