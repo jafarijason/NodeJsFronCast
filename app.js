81a2e9cee703150707358a420db2e2f09c80e053
thisisjasonjafari <jason.jafari.m@gmail.com>
2020-06-27 21:53:34 -0400

    add body parser to app

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-const path = require('path')
+const path = require("path");
 
 const express = require("express");
+const bodyParser = require("body-parser");
 
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 const app = express();
 
-app.use('/admin', adminRoutes)
-app.use(shopRoutes)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res) => {
-    res.status(404).send('Page Not Found.')
-})
+    res.status(404).send("Page Not Found.");
+});
 
 app.listen(3000);
