29f9497b78b0aa76747c5baa21091b31341a0fee
thisisjasonjafari <jason.jafari.m@gmail.com>
2020-06-28 12:45:14 -0400

    refactor-save-data-function

--- models/single-product.js
+++ models/single-product.js
@@ -1,6 +1,21 @@
 const fs = require("fs");
 const path = require("path");
 
+const filePath = path.join(
+    path.dirname(process.mainModule.filename),
+    "data",
+    "product.json"
+);
+
+const getProductsFromFile = (cb) => {
+    fs.readFile(filePath, (err, fileContent) => {
+        if (err) {
+            cb([]);
+        }
+        cb(JSON.parse(fileContent));
+    });
+};
+
 module.exports = class Product {
     constructor(title, description, price) {
         this.title = title;
@@ -9,17 +24,7 @@ module.exports = class Product {
     }
 
     saveProductData() {
-        const filePath = path.join(
-            path.dirname(process.mainModule.filename),
-            "data",
-            "product.json"
-        );
-
-        fs.readFile(filePath, (err, fileContent) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(fileContent);
-            }
+        getProductsFromFile((products) => {
             products.push(this);
 
             fs.writeFile(filePath, JSON.stringify(products), (err1) => {
@@ -29,16 +34,6 @@ module.exports = class Product {
     }
 
     static fetchAllProducts(cb) {
-        const filePath = path.join(
-            path.dirname(process.mainModule.filename),
-            "data",
-            "product.json"
-        );
-        fs.readFile(filePath, (err, fileContent) => {
-            if (err) {
-                cb([]);
-            }
-            cb(JSON.parse(fileContent));
-        });
+        getProductsFromFile(cb);
     }
 };
