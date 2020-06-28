624bfbe84dc582d872cc814f0dc61e54c50796cb
thisisjasonjafari <jason.jafari.m@gmail.com>
2020-06-28 13:33:06 -0400

    add-to-cart-controller-function

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,7 +1,5 @@
 const Product = require("../models/single-product");
 
-
-
 module.exports.getAllProducts = (req, res) => {
     Product.fetchAllProducts((products) => {
         res.render("shop/product-list", {
@@ -12,15 +10,14 @@ module.exports.getAllProducts = (req, res) => {
 };
 
 module.exports.getOneProduct = (req, res) => {
-    const pId = req.params.productId
+    const pId = req.params.productId;
     Product.fetchOneProduct(pId, (product) => {
-        res.render('shop/product-details', {
+        res.render("shop/product-details", {
             product: product,
-            pageTitle: `Product Deateil ${product.title}`
-        })
-    })
-
-}
+            pageTitle: `Product Deateil ${product.title}`,
+        });
+    });
+};
 
 module.exports.getIndex = (req, res) => {
     Product.fetchAllProducts((products) => {
@@ -29,23 +26,28 @@ module.exports.getIndex = (req, res) => {
             productsArray: products,
         });
     });
-}
+};
 
 module.exports.getCart = (req, res) => {
-    res.render('shop/cart', {
-        pageTitle: 'shop basket'
-    })
-}
+    res.render("shop/cart", {
+        pageTitle: "shop basket",
+    });
+};
 
+module.exports.postCart = (req, res) => {
+    const pId = req.body.productId;
+    console.log(pId);
+    res.redirect('/cart')
+};
 
 module.exports.getOrders = (req, res) => {
-    res.render('shop/orders', {
-        pageTitle: 'shop Orders'
-    })
-}
+    res.render("shop/orders", {
+        pageTitle: "shop Orders",
+    });
+};
 
 module.exports.getCheckout = (req, res) => {
-    res.render('shop/checkout', {
-        pageTitle: "Pay Order"
-    })
-}
+    res.render("shop/checkout", {
+        pageTitle: "Pay Order",
+    });
+};
