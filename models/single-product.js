feeb34212e7752802604ecca60487a4fcfaa3be5
thisisjasonjafari <jason.jafari.m@gmail.com>
2020-06-28 03:12:49 -0400

    add-product-attributes

--- models/single-product.js
+++ models/single-product.js
@@ -2,8 +2,10 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = class Product {
-    constructor(title) {
+    constructor(title, description, price) {
         this.title = title;
+        this.description = description;
+        this.price = price;
     }
 
     saveProductData() {
