a53b9a7e78463be6b2030cafa092a0d477e9b1fb
thisisjasonjafari <jason.jafari.m@gmail.com>
2020-06-27 22:26:37 -0400

    write ejs file for shop to show adding products

--- routes/admin.js
+++ routes/admin.js
@@ -9,16 +9,15 @@ const products = [];
 router.get("/add-product", (req, res) => {
     // res.send("add-product");
     // res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
-    res.render('add-product')
+    res.render("add-product");
 });
 
 router.post("/add-product", (req, res) => {
     products.push({ title: req.body.title });
-    console.log(products);
     res.redirect("/");
 });
 
 // module.exports = router;
 
-exports.routes = router
-exports.products = products
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -3,15 +3,16 @@ const path = require("path");
 const express = require("express");
 const adminData = require("./admin");
 
-const products = adminData.products;
-console.log(products);
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    // res.send("Shop Page");
-    //   res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
-    res.render('shop')
+    const products = adminData.products;
+
+    res.render('shop', {
+        pageTitle: 'فروشگاه',
+        productsArray: products
+    })
 });
 
 module.exports = router;
